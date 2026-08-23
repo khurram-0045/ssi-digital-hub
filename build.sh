d516cd284132eac53b89a58ce4ac44184af61c8f
Khurram@0045 <your_email@example.com>
2026-08-23 06:07:47 -0700

    Automate migrations and superuser creation in build script

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a default superuser if it doesn't exist
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin12345');
+    print('Superuser created successfully.');
+"
