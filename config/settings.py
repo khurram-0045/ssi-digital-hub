d83c159fef52aecfb3c784dfa3ef05e9826e51f4
Khurram@0045 <your_email@example.com>
2026-08-23 00:02:29 -0700

    Add STATIC_ROOT for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.1/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # <--- Add this line
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 # Email
